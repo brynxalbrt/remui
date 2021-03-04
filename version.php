793700663e4c8c25eecb376b792d57effa5ee1a3
Bryan Albert Isidro <bryanalbert31@gmail.com>
2021-03-04 11:55:29 +0800

    Adding version.php file

---
+++ version.php
@@ -0,0 +1,8 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version   = 2021010700;
+$plugin->requires  = 2018112800;
+$plugin->maturity  = MATURITY_STABLE; // This version's maturity level.
+$plugin->release   = '3.10.1';
+$plugin->component = 'theme_remui';
